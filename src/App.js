340f6a9a7f0f8be9a0460b90a450fc319b168ea3
flujan <flujan@eogresources.com>
2020-02-22 15:28:04 -0600

    Added navbar component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from "react";
 import "./App.scss";
 
+import Navbar from "./Components/Navbar/Navbar";
+
 function App() {
-  return <h1>City tours comming soon</h1>;
+  return (
+    <div>
+      <Navbar />
+      <h1>City tours comming soon</h1>
+    </div>
+  );
 }
 
 export default App;
