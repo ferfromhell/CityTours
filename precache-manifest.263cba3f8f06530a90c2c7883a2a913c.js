1bbe119391d2a966d2aa053a059b8775aaaea459
flujan <flujan@eogresources.com>
2020-02-23 19:14:28 -0600

    Updates

---
+++ precache-manifest.263cba3f8f06530a90c2c7883a2a913c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "589a017325e217d0f9e360054ce0ba3c",
+    "url": "/CityTours/index.html"
+  },
+  {
+    "revision": "46b5e7fb6e0a89775ab9",
+    "url": "/CityTours/static/css/main.626c3657.chunk.css"
+  },
+  {
+    "revision": "8fa7243bc37be6e658f5",
+    "url": "/CityTours/static/js/2.bcaf8325.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/CityTours/static/js/2.bcaf8325.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "46b5e7fb6e0a89775ab9",
+    "url": "/CityTours/static/js/main.66c1cf6e.chunk.js"
+  },
+  {
+    "revision": "23f8b00d42db10b017de",
+    "url": "/CityTours/static/js/runtime-main.3a3b3b90.js"
+  },
+  {
+    "revision": "9eafbbc07b5ee494e47460e47c337203",
+    "url": "/CityTours/static/media/combi-bg.9eafbbc0.jpg"
+  },
+  {
+    "revision": "ea3226d90b1f1edbaa78c241f8772393",
+    "url": "/CityTours/static/media/logo3.ea3226d9.png"
+  },
+  {
+    "revision": "51127b5a23add5c050332befd4a4d5a2",
+    "url": "/CityTours/static/media/map-bg.51127b5a.jpg"
+  }
+]);
